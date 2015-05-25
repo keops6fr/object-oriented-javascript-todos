2d63709bd36ee39425d89c17f710586326ebff4b
Benoit Demonsant & Coldhands <pair+keops6fr.supery4n@devbootcamp.com>
2015-05-25 16:34:26 -0700

    final

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,54 @@
-var newTodoList = function() {
-	// ???
+var TodoList = function() {
+  this.todos = [];
 };
 
+var Task = function(item) {
+    this.id = 1;
+    this.description = item;
+    this.completed =  false;
+};
 
+TodoList.prototype.add = function(item) {
+  var task = new Task(item);
+  this.todos.push(task);
+};
+
+TodoList.prototype.tasks = function() {
+  console.log(this.todos);
+  return this.todos
+};
+
+TodoList.prototype.list = function() {
+  for(var i = 0; i < this.todos.length; i++) {
+    console.log(this.todos[i]);
+  }
+};
+
+Task.prototype.complete = function() {
+  this.completed = true;
+};
+
+// TodoList.prototype.remove = function()
+
+TodoList.prototype.remove = function(removable) {
+  this.todos.splice(this.todos.indexOf(removable), 1)
+};
 
 // Driver code
+var groceryList = new TodoList();
+groceryList.add('bread');
+groceryList.add('cheese');
+groceryList.add('milk');
+groceryList.tasks();
+groceryList.list();
+var breadTask = groceryList.tasks()[0];
+console.log(breadTask)
+console.log(breadTask.id) //-> 1 (some unique numerical ID)
+console.log(breadTask.description) //-> 'bread'
+breadTask.complete()
+console.log(breadTask.completed) //->
+groceryList.list();
+groceryList.remove(breadTask);
+groceryList.list();
 
 
-var todoList = newTodoList();
